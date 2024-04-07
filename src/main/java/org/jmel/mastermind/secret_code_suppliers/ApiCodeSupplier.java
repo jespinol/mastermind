2187e645d407aed2877a0e63a289642fadcdd925
jespinol <76534045+jespinol@users.noreply.github.com>
2024-04-06 21:33:18 -0400

    Replaced RestClient with HttpClient to facilitate mocking API calls

--- src/main/java/org/jmel/mastermind/secret_code_suppliers/ApiCodeSupplier.java
+++ src/main/java/org/jmel/mastermind/secret_code_suppliers/ApiCodeSupplier.java
@@ -1,19 +1,20 @@
 package org.jmel.mastermind.secret_code_suppliers;
 
 import org.jmel.mastermind.Code;
-import org.springframework.http.client.JdkClientHttpRequestFactory;
-import org.springframework.web.client.RestClient;
 
 import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class ApiCodeSupplier implements CodeSupplier {
-    private static final String BASE_URL = "https://www.random.org";
-    private static final String QUOTA_URI = "/quota/?format=plain";
-    private static final String INTS_URI = "/integers/?num=%d&min=%d&max=%d&col=1&base=10&format=plain&rnd=new";
+    private static final String QUOTA_URI = "https://www.random.org/quota/?format=plain";
+    private static final String INTS_URI = "https://www.random.org/integers/?num=%d&min=%d&max=%d&col=1&base=10&format=plain&rnd=new";
     private final int codeLength;
     private final int numColors;
 
@@ -24,14 +25,14 @@ public class ApiCodeSupplier implements CodeSupplier {
 
     @Override
     public Code get() throws IOException {
-        RestClient restClient = buildRestClient();
-        // TODO test below with mock failures
         try {
-            if (checkQuota(restClient)) {
-                List<Integer> codeValue = getCodeFromApi(restClient);
+            HttpClient httpClient = buildHttpClient();
+            if (checkQuota(httpClient)) {
+                List<Integer> codeValue = getCodeFromApi(httpClient);
+
                 return Code.from(codeValue, codeLength, numColors);
             } else {
-                throw new IOException("Quota exceeded");
+                throw new IOException("Random.org quota exceeded");
             }
         } catch (RuntimeException e) {
             // Catches API exceptions (not including quota exceeded exception above) and rethrows them as IOExceptions
@@ -39,44 +40,50 @@ public class ApiCodeSupplier implements CodeSupplier {
         }
     }
 
-    private RestClient buildRestClient() {
-        // TODO: handle connection errors
-        JdkClientHttpRequestFactory clientHttpRequestFactory = new JdkClientHttpRequestFactory();
-        clientHttpRequestFactory.setReadTimeout(1000);
-
-        return RestClient
-                .builder()
-                .baseUrl(BASE_URL)
-                .requestFactory(clientHttpRequestFactory)
+    private HttpClient buildHttpClient() {
+        return HttpClient.newBuilder()
+                .connectTimeout(Duration.ofSeconds(1))
                 .build();
     }
 
-    private boolean checkQuota(RestClient restClient) {
-        // TODO: use quota check. E.g., a request for 4 integers from 0 to 7 costs 4*3 = 12 bits.
-        String quota = Objects.requireNonNull(restClient
-                .get()
-                .uri(QUOTA_URI)
-                .retrieve()
-                .body(String.class)).trim(); // Will throw a RuntimeException on 503
+    private boolean checkQuota(HttpClient client) {
+        try {
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(QUOTA_URI))
+                    .timeout(Duration.ofSeconds(1))
+                    .build();
 
-        return Integer.parseInt(quota) >= this.codeLength * 4; // TODO fix use the number of bits using base 2 log.
+            String quota = client.send(request, HttpResponse.BodyHandlers.ofString())
+                    .body()
+                    .trim();
+
+            // TODO: fix use the number of bits using base 2 log. e.g., a request for 4 integers from 0 to 7 costs 4*3 = 12 bits.
+            return Integer.parseInt(quota) >= this.codeLength * 4;
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    private List<Integer> getCodeFromApi(RestClient restClient) {
-        String result = Objects.requireNonNull(restClient
-                .get()
-                .uri(generateRequestPath())
-                .retrieve()
-                .body(String.class)); // Will throw a RuntimeException on 503
+    private List<Integer> getCodeFromApi(HttpClient client) {
+        try {
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(generateIntegerRequestPath()))
+                    .timeout(Duration.ofSeconds(1))
+                    .build();
 
-        System.out.println(BASE_URL + generateRequestPath());
+            String result = client.send(request, HttpResponse.BodyHandlers.ofString())
+                    .body()
+                    .trim();
 
-        return Arrays.stream(result.split("\n"))
-                .map(Integer::parseInt)
-                .collect(Collectors.toList());
+            return Arrays.stream(result.split("\n"))
+                    .map(Integer::parseInt)
+                    .collect(Collectors.toList());
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    private String generateRequestPath() {
+    private String generateIntegerRequestPath() {
         return String.format(INTS_URI, this.codeLength, 0, this.numColors - 1);
     }
 }
